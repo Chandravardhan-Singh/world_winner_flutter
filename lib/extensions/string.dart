074c1b17b10b4723df164fc3831856a22731fa3c
Chandravardhan-Singh <chandravardhan.singh@mutualmobile.com>
2023-07-12 19:49:03 +0530

    string ext updated for email

--- lib/extensions/string.dart
+++ lib/extensions/string.dart
@@ -31,4 +31,10 @@ extension AddStyle on String {
 
     return textSpans;
   }
+
+  bool isValidEmail() {
+    return RegExp(
+            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+        .hasMatch(this);
+  }
 }

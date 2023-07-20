171a48d001089e006cd23e069139d2162e20433d
Chandravardhan-Singh <chandravardhan.singh@mutualmobile.com>
2023-07-20 15:15:48 +0530

    saving user in secure store

--- lib/repository/onboarding_repository.dart
+++ lib/repository/onboarding_repository.dart
@@ -1,5 +1,6 @@
 import 'package:world_winner_flutter/models/country_code.dart';
 import 'package:world_winner_flutter/models/user.dart';
+import 'package:world_winner_flutter/utils/storage/storage.dart';
 
 enum SignupResponse { success, failed }
 
@@ -41,6 +42,11 @@ class UserOnboardingRepository {
 
   Future<SignupResponse> userSignup(User user) {
     return Future.delayed(const Duration(seconds: 1), () {
+      Storage.instance().add(
+        'user',
+        user.toString(),
+      );
+
       return SignupResponse.success;
     });
   }

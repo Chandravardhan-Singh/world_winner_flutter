e6a11773645aa225eccea3f6725b83725d804e52
Chandravardhan-Singh <chandravardhan.singh@mutualmobile.com>
2023-08-01 15:55:02 +0530

    created countdown class

---
+++ lib/models/countdown.dart
@@ -0,0 +1,13 @@
+class Countdown {
+  final String hour;
+  final String minutes;
+  final String days;
+  final String seconds;
+
+  Countdown({
+    required this.days,
+    required this.hour,
+    required this.minutes,
+    required this.seconds,
+  });
+}
